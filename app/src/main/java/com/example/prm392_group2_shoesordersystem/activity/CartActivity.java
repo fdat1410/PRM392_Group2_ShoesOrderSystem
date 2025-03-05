c05cd48c172d4987670af158515df5e2d290b7af
mitne22 <leminhnguyet22072003@gmail.com>
2025-03-06 00:04:00 +0700

    front-end

--- app/src/main/java/com/example/prm392_group2_shoesordersystem/activity/CartActivity.java
+++ app/src/main/java/com/example/prm392_group2_shoesordersystem/activity/CartActivity.java
@@ -1,4 +1,4 @@
-package com.example.prm392_group2_shoesordersystem;
+package com.example.prm392_group2_shoesordersystem.activity;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.prm392_group2_shoesordersystem.R;
+
+public class CartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cart);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
